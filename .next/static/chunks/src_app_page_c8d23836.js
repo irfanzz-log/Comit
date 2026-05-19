db3eea17df713b905d179367b9ce0a151f9ae893
irfanzz-log <syah90009@gmail.com>
2026-05-19 13:57:23 +0700

    Fix nav display build

--- .next/static/chunks/src_app_page_c8d23836.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_3fb59e91._.js",
-  "static/chunks/node_modules_bae5dfc3._.js"
-],
-    source: "dynamic"
-});
